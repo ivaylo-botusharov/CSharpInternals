8fb6be76cd2a0cbe197335fe82e7aa541f100301
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2024-04-08 15:55:54 +0300

    added C# Interceptors and Dynamic Proxies for Logging projects

---
+++ CSharp.Internals/Interceptors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+var customer = new Customer();
+customer.PlaceOrder(1); // invokes OrderInterceptor() and prints "Order interceptor: 1"
+customer.PlaceOrder(2); // invokes PlaceOrder() and prints "Order placed: 2"
+
+class Customer
+{
+    public void PlaceOrder(int orderId)
+    {
+        Console.WriteLine($"Order placed: {orderId}");
+    }
+}
+
+namespace Sales
+{
+    static class OrderExtensions
+    {
+        [InterceptsLocation("Program.cs", line: 5, character: 10)]
+        public static void OrderInterceptor(this Customer customer, int orderId)
+        {
+            Console.WriteLine($"Order interceptor: {orderId}");
+        }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class InterceptsLocationAttribute(string filePath, int line, int character) : Attribute
+    {
+    }
+}
---
+++ CSharp.Internals/ProxiesForLogging/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Castle.DynamicProxy;
+
+using Clients;
+using Interceptors;
+
+ServiceProvider serviceProvider = ConfigureServices();
+
+var customer = serviceProvider.GetRequiredService<ICustomer>();
+customer.PlaceOrder(123);
+
+static ServiceProvider ConfigureServices()
+{
+    var services = new ServiceCollection();
+
+    services.AddLogging(loggingBuilder =>
+    {
+        loggingBuilder.ClearProviders();
+        loggingBuilder.AddConsole();
+    });
+
+    services.AddTransient<IProxyGenerator, ProxyGenerator>();
+    services.AddTransient<LoggingInterceptor>();
+
+    services.AddTransient<ICustomer>(provider => 
+    {
+        var customer = new Customer();
+        var loggingInterceptor = provider.GetRequiredService<LoggingInterceptor>();
+        
+        var proxyGenerator = provider.GetRequiredService<IProxyGenerator>();
+
+        ICustomer customerProxy = proxyGenerator.CreateInterfaceProxyWithTarget<ICustomer>(
+            customer,
+            loggingInterceptor);
+
+        return customerProxy;
+    });
+
+
+    ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+    return serviceProvider;
+}
+
+namespace Clients
+{
+    public interface ICustomer
+    {
+        void PlaceOrder(int orderId);
+    }
+
+    public class Customer: ICustomer
+    {
+        public void PlaceOrder(int orderId)
+        {
+            Console.WriteLine($"Placing order: {orderId}");
+        }
+    }
+}
+
+namespace Interceptors
+{
+    public class LoggingInterceptor : IInterceptor
+    {
+        private readonly ILogger logger;
+
+        public LoggingInterceptor(ILogger<LoggingInterceptor> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            this.logger.LogInformation($"Entering {invocation.Method.Name}");
+            invocation.Proceed();
+            this.logger.LogInformation($"Exiting {invocation.Method.Name}");
+        }
+    }
+}
