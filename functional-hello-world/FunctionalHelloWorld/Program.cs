6e35b08381416b9d140167ac90e553e598ec1daa
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2024-04-20 17:10:19 +0300

    replaced lambda with method group

--- functional-hello-world/FunctionalHelloWorld/Program.cs
+++ functional-hello-world/FunctionalHelloWorld/Program.cs
@@ -1,13 +1,14 @@
-﻿using LanguageExt;
+﻿using static System.Console;
+using LanguageExt;
 using LanguageExt.Common;
 
-ProductService productService = new ProductService();
+ProductService productService = new();
 
 Fin<Either<Error, string>> productDetails = await productService
     .GetProductDetailsAsync("123")
     .Run();
 
 productDetails.Match(
-    Succ: either => either.IfRight(details => Console.WriteLine(details)),
-    Fail: error => Console.WriteLine($"Error: {error.Message}")
+    Succ: either => either.IfRight(WriteLine),
+    Fail: error => WriteLine($"Error: {error.Message}")
 );
